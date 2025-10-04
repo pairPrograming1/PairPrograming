f3300dcf3e74a69e77142e48fd31d1cba38e121c
Pablito471 <pablohernanbrito@gmail.com>
2025-10-04 18:58:23 -0300

    creacion

--- pairprogramming-web/src/app/page.jsx
+++ pairprogramming-web/src/app/page.jsx
@@ -1,3 +1,4 @@
+// app/page.js
 import Sidebar from "../components/Sidebar";
 import Hero from "../components/Hero";
 import Services from "../components/Services";
@@ -9,7 +10,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Sidebar />
-      <main className="ml-64 flex flex-col">
+      <main className="lg:ml-64 flex flex-col">
         <Hero />
         <Services />
         <Values />
