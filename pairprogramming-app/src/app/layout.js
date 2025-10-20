41537c01454692ed2e54bc2ed6108dfd5dfea58f
estebanaleart <esteban.aleart@gmail.com>
2025-10-20 11:50:26 +0200

    googletag

--- pairprogramming-app/src/app/layout.js
+++ pairprogramming-app/src/app/layout.js
@@ -4,6 +4,7 @@ import "./globals.css";
 import { SidebarProvider } from "./context/SidebarContext";
 import FloatingWidgets from "./components/FloatingWidgets";
 import Header from "./components/Header";
+import Script from "next/script";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -16,10 +17,46 @@ export const metadata = {
     "Co-creamos soluciones digitales que integran tecnología y estrategia",
 };
 
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="es" className="dark">
+//       <body className={`${inter.variable} antialiased`}>
+//         <SidebarProvider>
+//           <Header />
+//           {children}
+//           <FloatingWidgets />
+//         </SidebarProvider>
+//       </body>
+//     </html>
+//   );
+// }
 export default function RootLayout({ children }) {
   return (
     <html lang="es" className="dark">
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-W9B7K49F');`}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
+
       <body className={`${inter.variable} antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W9B7K49F"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <SidebarProvider>
           <Header />
           {children}
